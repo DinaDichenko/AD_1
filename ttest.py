5fe4459ca146507fbc192a331575ac7736cd284a
student-09-510 <dinadichenko@mail.ru>
2023-09-06 10:59:28 +0300

    'pyfile'

--- ttest.py
+++ ttest.py
@@ -3,5 +3,5 @@ import scipy
 import csv
 
 
-with open('C:/Users/student-09-510/Downloads/archive/T-test-2/Heart_Attack_Data_Set.csv', 'r', newline='') as csvfile:
+with open('Heart_Attack_Data_Set.csv', 'r', newline='') as csvfile:
     
